b5341e41b9d542dd4883d5967e92dd04842f5efa
Breno Thales <breno.thales@gmail.com>
2015-03-25 09:49:24 -0500

    Added flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show]
+
+  
+  def index
+  	@users = User.all
+  end
   
   def show
   end
@@ -7,6 +12,6 @@ class UsersController < ApplicationController
   private 
 
   def set_user
-  	@user = User.find_by(username: params[:id])  	
+  	  @user = User.find_by(username: params[:id])	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {registrations: 'registrations'}
+  devise_for :users, controllers: {registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
-   resources :users, only: [:show] 
+   resources :users, only: [:show, :index] 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

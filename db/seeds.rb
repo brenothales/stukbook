93910f2e94032c4b717dc24520d552b691e07202
Breno Thales <breno.thales@gmail.com>
2015-03-25 10:20:55 -0500

    Added database seed all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "sandramarta", email:"sandramarta@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+User.create(username: "sandramarta1", email:"sandramarta1@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+User.create(username: "sandramarta2", email:"sandramarta2@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+User.create(username: "sandramarta3", email:"sandramarta3@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+User.create(username: "sandramarta4", email:"sandramarta4@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+User.create(username: "sandramarta5", email:"sandramarta5@gmail.com", password: "thalesWeb", password_confirmation: "thalesWeb")
+
+p "Teste users created"
